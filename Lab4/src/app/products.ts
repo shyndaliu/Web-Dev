4b73cbf81677db48492bd6a7156915c8b583fde7
uldana shyndali <122513545+shyndaliu@users.noreply.github.com>
2024-02-16 01:44:18 +0600

    finished 4th lab

---
+++ Lab4/src/app/products.ts
@@ -0,0 +1,102 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'Восхитительное бисквитное пирожное "Медвежонок Барни" с густой молочной начинкой сделает вашего ребенка счастливым! Замечательный шоколадный медвежонок со светлыми лапками подарит удивительный вкус и необычный сливочный аромат.',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  },
+  {
+    id: 2,
+    name: 'Мягкая игрушка Kuromi 1885-21,',
+    price: 3489,
+    description: 'мягкая игрушка',
+    rating: 4.1,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/hc3/h21/83840363495454.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/mjagkaja-igrushka-kuromi-1885-21-vysota-30-sm-mul-tikolor-113374385/?c=750000000'
+  },
+  {
+    id: 3,
+    name: 'Корм Felix кусочки в желе говядина 75 г 1 шт',
+    price: 149,
+    description: 'рацион необходимо корректировать в зависимости от погодных условий, уровня активности и физического состояния, всегда должен быть доступ к свежей чистой питьевой воде',
+    rating: 4.9,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h3b/h01/84564384645150.png?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/felix-kusochki-v-zhele-govjadina-75-g-1-sht-107965425/?c=750000000'
+  },
+  {
+    id: 4,
+    name: 'Печенье Milka Choco Cow шоколадное 200 г',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 5.0,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h76/hab/84983106371614.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/pechen-e-milka-choco-cow-shokoladnoe-200-g-100971310/?c=750000000'
+  },
+  {
+    id: 5,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  },
+  {
+    id: 6,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  }, {
+    id: 7,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  }, {
+    id: 8,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  }, {
+    id: 9,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  }, {
+    id: 10,
+    name: 'Medvejonok barni',
+    price: 599,
+    description: 'страна производства: Казахстан',
+    rating: 4.5,
+    img: 'https://resources.cdn-kaspi.kz/img/m/p/h77/h18/84983107223582.jpg?format=gallery-medium',
+    link: 'https://kaspi.kz/shop/p/medvezhonok-barni-biskvit-s-molochnoi-nachinkoi-150-g-101179235/?c=750000000'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
